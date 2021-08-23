8f5b566cb38f98ab74b309ff5c12a0d86e27c236
Corey Miller <coreymiller454@gmail.com>
2021-08-22 23:21:02 -0400

    add ability to add comment to post

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -5,21 +5,64 @@ import { useParams } from "react-router";
 const Post = () => {
 	let { id } = useParams();
 	const [postObject, setPostObject] = useState({});
+	const [comments, setComments] = useState([]);
+	const [newComment, setNewComment] = useState("");
 
 	useEffect(() => {
 		axios.get(`http://localhost:3001/posts/byId/${id}`).then((response) => {
 			setPostObject(response.data);
 		});
-	});
+
+		axios.get(`http://localhost:3001/comments/${id}`).then((response) => {
+			setComments(response.data);
+		});
+	}, [id]);
+
+	const addComment = () => {
+		axios
+			.post("http://localhost:3001/comments", {
+				commentBody: newComment,
+				PostId: id,
+			})
+			.then((response) => {
+				const commentToAdd = { commentBody: newComment };
+				setComments([...comments, commentToAdd]);
+				setNewComment("");
+			});
+	};
 
 	return (
 		<div className="postPage">
 			<div className="leftSide">
-				<div className="title">{postObject.title}</div>
-				<div className="postText">{postObject.postText}</div>
-				<div className="footer">{postObject.username}</div>
+				<div className="post" id="individual">
+					<div className="title">{postObject.title}</div>
+					<div className="body">{postObject.postText}</div>
+					<div className="footer">{postObject.username}</div>
+				</div>
 			</div>
-			<div className="rightSide"></div>
+			<div className="rightSide">
+				<div className="addCommentContainer">
+					<input
+						type="text"
+						placeholder="Comment..."
+						autoComplete="off"
+						value={newComment}
+						onChange={(event) => {
+							setNewComment(event.target.value);
+						}}
+					/>
+					<button onClick={addComment}>Add Comment</button>
+				</div>
+				<div className="listOfComments">
+					{comments.map((value, index) => {
+						return (
+							<div key={index} className="comment">
+								{value.commentBody}
+							</div>
+						);
+					})}
+				</div>
+			</div>
 		</div>
 	);
 };

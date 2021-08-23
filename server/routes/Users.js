4ec5ce1d7791e987c1429ee1a25213d9755fbb29
Corey Miller <coreymiller454@gmail.com>
2021-08-23 15:49:09 -0400

    add login route

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,9 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 const { Users } = require("../models");
 
 router.post("/", async (req, res) => {
 	const { username, password } = req.body;
+	bcrypt.hash(password, 10).then((hash) => {
+		Users.create({
+			username: username,
+			password: hash,
+		});
+	});
+	res.json("Success");
+});
+
+router.post("/login", async (req, res) => {
+	const { username, password } = req.body;
+	const user = await Users.findOne({
+		where: {
+			username: username,
+		},
+	});
+	if (!user) {
+		res.json({ error: "Incorrect User" });
+	} else {
+		bcrypt.compare(password, user.password).then((match) => {
+			if (!match)
+				res.json({ error: "Wrong username and password combination" });
+			res.json({
+				message: "You are logged in.",
+			});
+		});
+	}
 });
 
 module.exports = router;

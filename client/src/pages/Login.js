ee6ccaf7c3eb21be0534d35f5fcb13c2cd8e2fd2
Corey Miller <coreymiller454@gmail.com>
2021-08-23 16:08:48 -0400

    add login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
-	return <div>Login</div>;
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	const login = () => {};
+
+	return (
+		<div>
+			<input
+				type="text"
+				name=""
+				id=""
+				onChange={(event) => setUsername(event.target.value)}
+				value={username}
+			/>
+			<input
+				type="password"
+				name=""
+				id=""
+				onChange={(event) => setPassword(event.target.value)}
+				value={password}
+			/>
+			<button type="submit" onSubmit={login}>
+				Login
+			</button>
+		</div>
+	);
 }
 
 export default Login;
